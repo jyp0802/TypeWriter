b898815797b0177d5141458080ff781d681cd525
gicheonkang <kgc519@gmail.com>
2017-09-16 04:42:28 +0900

    image parser modified(black alphabet, white background)

--- image_parser.py
+++ image_parser.py
@@ -7,19 +7,26 @@ original_image = Image.open('original.png')
 
 # pixeling, array
 original_array = np.array(original_image)
-pixels = original_image.load()
+original_pixels = original_image.load()
 
 # background color with white, alphabet with red
 for i in range(original_array.shape[1]):
     for j in range(original_array.shape[0]):
-        if pixels[i, j][0] + pixels[i, j][1] + pixels[i, j][2] < 400:
-            pixels[i, j] = (255, 0, 0)
+        if original_pixels[i, j][0] + original_pixels[i, j][1] + original_pixels[i, j][2] < 400:
+            original_pixels[i, j] = (0, 0, 0)
         else:
-            pixels[i, j] = (255, 255, 255)
+            original_pixels[i, j] = (255, 255, 255)
+
+
+# modified image with black, white background
+scipy.misc.imsave("modified.png", original_image)
+modified_image = Image.open("modified.png")
+modified_array = np.array(modified_image)
+modified_pixels = modified_image.load()
 
 # alphabet parsing
-print (original_array.shape[0])
-print (original_array.shape[1])
+print (modified_array.shape[0])
+print (modified_array.shape[1])
 
 isDetected = False
 num_of_white = 0
@@ -30,9 +37,9 @@ horizontal_max = 0
 index = 0
 
 
-for i in range(original_array.shape[1]):
-    for j in range(original_array.shape[0]):
-        if pixels[i, j][1] == 0:
+for i in range(modified_array.shape[1]):
+    for j in range(modified_array.shape[0]):
+        if modified_pixels[i, j][1] == 0:
             isDetected = True
             ver_min = min(ver_min, j)
             ver_max = max(ver_max, j)
@@ -42,9 +49,9 @@ for i in range(original_array.shape[1]):
             num_of_white = num_of_white + 1
 
 
-    if (num_of_white == original_array.shape[0]) and isDetected:
+    if (num_of_white == modified_array.shape[0]) and isDetected:
         #print (ver_min, ver_max, horizontal_min, horizontal_max)
-        img = original_array[ver_min:ver_max, horizontal_min:horizontal_max]
+        img = modified_array[ver_min:ver_max, horizontal_min:horizontal_max]
         scipy.misc.imsave(chr(97+index)+'.png', img)
         index = index + 1
         isDetected = False
