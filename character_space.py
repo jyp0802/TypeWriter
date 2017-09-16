cb1bb8fc7f98c1c724ea294ff59dbfe7374e76d9
Junyong Park <jyp0802@hotmail.com>
2017-09-16 15:11:34 +0900

    horizontal space

--- character_space.py
+++ character_space.py
@@ -1,51 +1,99 @@
 from PIL import Image
-img = Image.open("inp.png")
-txt = "aabacadaeafagahaiajakalamanaoapaqarasa"
-w, h = img.size
+import os
 
-p_img = img.load()
+txt = ["aabacadaeagahaiajakalamanaoapaqarasa","tauavawaxayaza",
+       "abbcbdbebfbgbhbibjbkblbmbnbobqbrbsbtbubvbwbx","xbybzb",
+       "bccdcecfcgchcicjckclcmcncocpcqcrcsctcucvcw","cxcyczc",
+       "cddedfdgdhdidjdkdldmdndodpdqdrdsdtdudvdwd","xdydzd",
+       "deefegeheiejekelemeneoepeqereseteuevewexe","effgfhfifjfkflmfnfofpfqfrfsftfufvfwfxfyf",
+       "zf","fgghgigjgkglgmgngogpgqgrgsgtgugvgwgxgygzg",
+       "hiijikiliminioipiqirisitiuiviwixiyizi","ijjkjljmjnjojpjqjrjsjtjujvjwjxjyjzj",
+       "yeze","jkklkmknkokpkqkrksktkukvkwkxkykzk",
+       "kllmlnlolplqlrlsltlulvlwlxlylzl","lmmnmompmqmrmsmtmumvmwmxmymzm",
+       "mnnonpnqnrnsntnunvnwnxnynzn","noopoqorosotouovowoxoyozo",
+       "oppqprpsptpupvpwpxpypzp","pqqrqsqtquqvqwqxqyqzq",
+       "qrrsrtrurvrwrxryrzr","rsstsusvswsxsyszs",
+       "sttutvtwtxtytzt","tuuvwuxuyuzu",
+       "vwwxwywzw","wxxyxzxyyzyzz",
+       "ghhihjhkhlhmhnhohphqhrhshthuhvhwhxhyhzh"]
 
-data = [] #[hor dist, ver dist, cnt]
-for i in range(26):
-    data.append([])
-    for j in range(26):
-        data[i].append([0,0,0])
+if (os.path.isfile('res')):
+    print "ASD"
+    afile = open('res', 'r')
+    adata = afile.read();
+    adata = adata.split();
+    data = []
+    for i in range(26):
+        data.append([])
+        for j in range(26):
+            data[i].append([int(adata[(3*j)+(26*i)]),int(adata[(3*j)+(26*i)+1]),int(adata[(3*j)+(26*i)+2])])
+else:
+    data = []  # [hor dist, ver dist, cnt]
+    for i in range(26):
+        data.append([])
+        for j in range(26):
+            data[i].append([0, 0, 0])
 
-cc = 0
-tmp = False
-ht = False
+for pic in range(len(txt)):
+    path = "in" + str(pic + 1) + ".png"
+    img = Image.open(path)
+    w, h = img.size
 
-dt = [] #[hor start, hor end, ver start, ver end]
-for j in range(len(txt)):
-    dt.append([0,0,0,0])
+    p_img = img.load()
 
-for i in range(w):
+    cc = 0
+    tmp = False
     ht = False
-    for j in range(h):
-        r,g,v = p_img[i,j]
-        if (r+g+v<200):
-            if (not tmp):
-                tmp = True
-                dt[cc][0] = i
-                dt[cc][2] = j
-                dt[cc][3] = j
-            ht = True
-            if (j > dt[cc][2]):
-                dt[cc][2] = j
-            elif (j < dt[cc][3]):
-                dt[cc][3] = j
-        elif ((j == h-1) and (not ht) and tmp):
-            tmp = False
-            dt[cc][1] = i-1
-            cc += 1
+
+    dt = [] #[hor start, hor end, ver start, ver end]
+    for j in range(len(txt[pic])):
+        dt.append([0,0,0,0])
+
+    for i in range(w):
+        ht = False
+        for j in range(h):
+            P = p_img[i,j]
+            if (P[0]+P[1]+P[2]<400):
+                if (not tmp):
+                    tmp = True
+                    dt[cc][0] = i
+                    dt[cc][2] = j
+                    dt[cc][3] = j
+                ht = True
+                if (j > dt[cc][2]):
+                    dt[cc][2] = j
+                elif (j < dt[cc][3]):
+                    dt[cc][3] = j
+            elif ((j == h-1) and (not ht) and tmp):
+                tmp = False
+                dt[cc][1] = i-1
+                cc += 1
+    if (not (cc ==len(txt[pic]))):
+        print "ERROR " + str(cc) + " " +str(len(txt[pic]))
+        for a in range(h):
+            for b in range(w):
+                P = p_img[b, a]
+                if (P[0] + P[1] + P[2] < 400):
+                    print 0,
+                else:
+                    print " ",
+            print ""
+        break
 
 
-for c in range(1,len(txt)):
-    x = ord(txt[c-1]) - ord('a')
-    y = ord(txt[c]) - ord('a')
-    data[x][y][0] += dt[c][0] - dt[c-1][1]
-    #data[x][y][1] += dt[c][0] - dt[c-1][1]
-    data[x][y][2] += 1
+    for c in range(1,len(txt[pic])):
+        x = ord(txt[pic][c-1]) - ord('a')
+        y = ord(txt[pic][c]) - ord('a')
+        data[x][y][0] += dt[c][0] - dt[c-1][1]
+        #data[x][y][1] += dt[c][0] - dt[c-1][1]
+        data[x][y][2] += 1
 
-print dt
-print data[0]
+bfile = open('res', 'w')
+
+for i in range(26):
+    for j in range(26):
+        # if (data[i][j][2] > 0):
+        #     data[i][j][0] = data[i][j][0] / data[i][j][2]
+        bfile.write(str(data[i][j][0]) + " " + str(data[i][j][1]) + " " + str(data[i][j][2]) + " ")
+
+bfile.close()
