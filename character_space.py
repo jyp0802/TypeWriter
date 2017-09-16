7322e7f1aec99f8f26443005eb2162f96e2a53ae
cch98 <changho9808@gmail.com>
2017-09-16 18:39:45 +0900

    Merge branch 'master' of https://github.com/jyp0802/TypeWriter
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- character_space.py
+++ character_space.py
@@ -1,51 +1,86 @@
 from PIL import Image
-img = Image.open("inp.png")
-txt = "aabacadaeafagahaiajakalamanaoapaqarasa"
-w, h = img.size
+import os
 
-p_img = img.load()
+txt = ["aabacadaeagahaiajakalamanaoapaqarasa","tauavawaxayaza",
+       "abbcbdbebfbgbhbibjbkblbmbnbobqbrbsbtbubvbwbx","xbybzb",
+       "bccdcecfcgchcicjckclcmcncocpcqcrcsctcucvcw","cxcyczc",
+       "cddedfdgdhdidjdkdldmdndodpdqdrdsdtdudvdwd","xdydzd",
+       "deefegeheiejekelemeneoepeqereseteuevewexe","effgfhfifjfkflmfnfofpfqfrfsftfufvfwfxfyf",
+       "zf","fgghgigjgkglgmgngogpgqgrgsgtgugvgwgxgygzg",
+       "hiijikiliminioipiqirisitiuiviwixiyizi","ijjkjljmjnjojpjqjrjsjtjujvjwjxjyjzj",
+       "yeze","jkklkmknkokpkqkrksktkukvkwkxkykzk",
+       "kllmlnlolplqlrlsltlulvlwlxlylzl","lmmnmompmqmrmsmtmumvmwmxmymzm",
+       "mnnonpnqnrnsntnunvnwnxnynzn","noopoqorosotouovowoxoyozo",
+       "oppqprpsptpupvpwpxpypzp","pqqrqsqtquqvqwqxqyqzq",
+       "qrrsrtrurvrwrxryrzr","rsstsusvswsxsyszs",
+       "sttutvtwtxtytzt","tuuvwuxuyuzu",
+       "vwwxwywzw","wxxyxzxyyzyzz",
+       "ghhihjhkhlhmhnhohphqhrhshthuhvhwhxhyhzh"]
 
-data = [] #[hor dist, ver dist, cnt]
+if (os.path.isfile('res')):
+    prev_file = open('res', 'r')
+    prev_data = prev_file.read();
+    prev_data = prev_data.split();
+    data = []
+    for i in range(26):
+        data.append([])
+        for j in range(26):
+            data[i].append([int(prev_data[(3*j)+(3*26*i)]),int(prev_data[(3*j)+(3*26*i)+1]),int(prev_data[(3*j)+(3*26*i)+2])])
+    prev_file.close()
+else:
+    data = []  # [hor dist, ver dist, cnt]
+    for i in range(26):
+        data.append([])
+        for j in range(26):
+            data[i].append([0, 0, 0])
+
+for pic in range(len(txt)):
+    path = "./input_data/in" + str(pic + 1) + ".png"
+    img = Image.open(path)
+    w, h = img.size
+
+    p_img = img.load()
+
+    char_cnt = 0
+    on_letter = False
+    exist_black = False
+
+    letter_data = [] #[hor start, hor end, ver start, ver end]
+    for j in range(len(txt[pic])):
+        letter_data.append([0,0,0,0])
+
+    for i in range(w):
+        exist_black = False
+        for j in range(h):
+            P = p_img[i,j]
+            if (P[0]+P[1]+P[2]<400):
+                if (not on_letter):
+                    on_letter = True
+                    letter_data[char_cnt][0] = i
+                    letter_data[char_cnt][2] = j
+                    letter_data[char_cnt][3] = j
+                exist_black = True
+                if (j > letter_data[char_cnt][2]):
+                    letter_data[char_cnt][2] = j
+                elif (j < letter_data[char_cnt][3]):
+                    letter_data[char_cnt][3] = j
+            elif ((j == h-1) and (not exist_black) and on_letter):
+                on_letter = False
+                letter_data[char_cnt][1] = i-1
+                char_cnt += 1
+
+
+    for c in range(1,len(txt[pic])):
+        x = ord(txt[pic][c-1]) - ord('a')
+        y = ord(txt[pic][c]) - ord('a')
+        data[x][y][0] += letter_data[c][0] - letter_data[c-1][1]
+        #data[x][y][1] += letter_data[c][0] - letter_data[c-1][1]
+        data[x][y][2] += 1
+
+result_file = open('res', 'w')
+
 for i in range(26):
-    data.append([])
     for j in range(26):
-        data[i].append([0,0,0])
+        result_file.write(str(data[i][j][0]) + " " + str(data[i][j][1]) + " " + str(data[i][j][2]) + " ")
 
-cc = 0
-tmp = False
-ht = False
-
-dt = [] #[hor start, hor end, ver start, ver end]
-for j in range(len(txt)):
-    dt.append([0,0,0,0])
-
-for i in range(w):
-    ht = False
-    for j in range(h):
-        r,g,v = p_img[i,j]
-        if (r+g+v<200):
-            if (not tmp):
-                tmp = True
-                dt[cc][0] = i
-                dt[cc][2] = j
-                dt[cc][3] = j
-            ht = True
-            if (j > dt[cc][2]):
-                dt[cc][2] = j
-            elif (j < dt[cc][3]):
-                dt[cc][3] = j
-        elif ((j == h-1) and (not ht) and tmp):
-            tmp = False
-            dt[cc][1] = i-1
-            cc += 1
-
-
-for c in range(1,len(txt)):
-    x = ord(txt[c-1]) - ord('a')
-    y = ord(txt[c]) - ord('a')
-    data[x][y][0] += dt[c][0] - dt[c-1][1]
-    #data[x][y][1] += dt[c][0] - dt[c-1][1]
-    data[x][y][2] += 1
-
-print dt
-print data[0]
+result_file.close()
