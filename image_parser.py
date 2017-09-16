1e2001328c72122ddc1398d331952c8474c82812
Junyong Park <jyp0802@hotmail.com>
2017-09-16 21:58:45 +0900

    capital letters included

--- image_parser.py
+++ image_parser.py
@@ -9,13 +9,24 @@ original_image = Image.open('original3.png')
 original_array = np.array(original_image)
 original_pixels = original_image.load()
 
+# divide image to top and bottom row -> abc.. & ABC..
+firstRow = False
+noBlack = True
+middle = -1;
+
 # background color with white, alphabet with red
-for i in range(original_array.shape[1]):
-    for j in range(original_array.shape[0]):
+for j in range(original_array.shape[0]):
+    for i in range(original_array.shape[1]):
         if original_pixels[i, j][0] + original_pixels[i, j][1] + original_pixels[i, j][2] < 360:
             original_pixels[i, j] = (0, 0, 0)
+            firstRow = True
+            noBlack = False
         else:
             original_pixels[i, j] = (255, 255, 255)
+    if (middle < 0 and firstRow and noBlack):
+    		middle = j+1
+    noBlack = True
+
 
 
 # modified image with black, white background
@@ -35,30 +46,33 @@ ver_max = 0
 horizontal_min = 10000
 horizontal_max = 0
 index = 0
+ascii_base = [97,65]
+height = [0,middle,modified_array.shape[0]]
+width = modified_array.shape[1]
+
+for t in range(2):
+		for i in range(width):
+		    for j in range(height[t],height[t+1]):
+		        if modified_pixels[i, j][1] == 0:
+		            isDetected = True
+		            ver_min = min(ver_min, j)
+		            ver_max = max(ver_max, j)
+		            horizontal_min = min(horizontal_min, i)
+		            horizontal_max = max(horizontal_max, i)
+		        else:
+		            num_of_white = num_of_white + 1
 
 
-for i in range(modified_array.shape[1]):
-    for j in range(modified_array.shape[0]):
-        if modified_pixels[i, j][1] == 0:
-            isDetected = True
-            ver_min = min(ver_min, j)
-            ver_max = max(ver_max, j)
-            horizontal_min = min(horizontal_min, i)
-            horizontal_max = max(horizontal_max, i)
-        else:
-            num_of_white = num_of_white + 1
-
-
-    if (num_of_white == modified_array.shape[0]) and isDetected:
-        #print (ver_min, ver_max, horizontal_min, horizontal_max)
-        img = modified_array[ver_min:ver_max, horizontal_min:horizontal_max]
-        scipy.misc.imsave(chr(97+index)+'.png', img)
-        index = index + 1
-        isDetected = False
-        num_of_white = 0
-        ver_min = 10000
-        ver_max = 0
-        horizontal_min = 10000
-        horizontal_max = 0
-    num_of_white = 0
+		    if (num_of_white == height[t+1]) and isDetected:
+		        #print (ver_min, ver_max, horizontal_min, horizontal_max)
+		        img = modified_array[ver_min:ver_max, horizontal_min:horizontal_max]
+		        scipy.misc.imsave(chr(ascii_base[t]+index)+'.png', img)
+		        index = index + 1
+		        isDetected = False
+		        num_of_white = 0
+		        ver_min = 10000
+		        ver_max = 0
+		        horizontal_min = 10000
+		        horizontal_max = 0
+		    num_of_white = 0
 
